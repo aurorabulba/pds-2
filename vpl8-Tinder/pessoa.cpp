d9c0fad45c6afd8eea81337fd666e5bae466fed9
Aurora Bombassaro <aurorabombassaro@gmail.com>
2025-05-22 21:32:10 -0300

    vpl8

---
+++ vpl8-Tinder/pessoa.cpp
@@ -0,0 +1,27 @@
+/* Includes */
+//...
+#include "pessoa.hpp"
+
+
+//format_str
+// Converte a primeira letra para maiúscula e o restante para minúscula
+void Pessoa::format_str(std::string &str) {
+  // Converte a primeira letra para maiúscula e o restante para minúscula
+  // std::transform(str.begin(), str.begin()+1, str.begin(), ::toupper);
+  //continua ...
+}
+
+
+// Construtor de Pessoa
+// Não esqueça de inicializar o ponteiro caract com o operador new
+// e de formatar o nome
+
+
+
+
+// print
+// Impressão dos dados da pessoa
+// Utilize o map para converter os valores de pele e tipoFisico
+
+//
+
---
+++ vpl8-Tinder/tinder.hpp
@@ -0,0 +1,12 @@
+#include <string>
+#include <map>
+#include <vector>
+struct caractFis{
+    /*Valores válidos:
+    sexo: M: Masculino, F: Feminino
+    pele: M: Morena, L: Loira
+    tipoFisico: M: Magro, E: Esbelta, O: Obeso*/
+    char pele, tipoFisico, pele;
+    caractFis(char sex, char pel, char tpf);
+    caractFis() = default;
+};
